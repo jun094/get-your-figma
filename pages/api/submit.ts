5d6b228baf590ea91a0285a96193deea5751a8ab
jun-young <jun094@naver.com>
2022-03-15 23:04:32 +0900

    test: test 커밋

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -11,34 +11,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     console.log(body);
 
     try {
-        //     const auth = new google.auth.GoogleAuth({
-        //         credentials: {
-        //             client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        //             private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        //         },
-        //         scopes: [
-        //             'https://www.googleapis.com/auth/drive',
-        //             'https://www.googleapis.com/auth/drive.file',
-        //             'https://www.googleapis.com/auth/spreadsheets',
-        //         ],
-        //     });
+        const auth = new google.auth.GoogleAuth({
+            credentials: {
+                client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            },
+            scopes: [
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.file',
+                'https://www.googleapis.com/auth/spreadsheets',
+            ],
+        });
 
-        //     const sheets = google.sheets({
-        //         auth,
-        //         version: 'v4',
-        //     });
+        const sheets = google.sheets({
+            auth,
+            version: 'v4',
+        });
 
-        //     const response = await sheets.spreadsheets.values.append({
-        //         spreadsheetId: process.env.GOOGLE_SHEET_ID,
-        //         range: 'A1:E1',
-        //         valueInputOption: 'USER_ENTERED',
-        //         requestBody: {
-        //             values: [[body.node_id, body.name, body.ko, body.en, body.ja]],
-        //         },
-        //     });
+        const response = await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            range: 'A1:E1',
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+                values: [[body.node_id, body.name, body.ko, body.en, body.ja]],
+            },
+        });
 
         return res.status(201).json({
-            data: body,
+            data: response.data,
         });
     } catch (err: unknown) {
         if (err instanceof Error) {
