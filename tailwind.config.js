a5dc26de6c8ef5e1b6679d7d16554eade2325cf2
jun094 <jun094@naver.com>
2023-01-21 22:52:46 +0900

    style: daisyUI 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,28 +8,6 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    darkTheme: 'dark',
-    themes: [
-      {
-        mytheme: {
-          primary: '#570DF8',
-          secondary: '#F000B8',
-          accent: '#37CDBE',
-          neutral: '#3D4451',
-          'base-100': '#FFFFFF',
-          info: '#3ABFF8',
-          success: '#36D399',
-          warning: '#FBBD23',
-          error: '#F87272',
-        },
-      },
-    ],
+    themes: ['dracula'],
   },
 }
